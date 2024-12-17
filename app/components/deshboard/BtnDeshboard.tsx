53f0dc340ca210358c9341ad3c75ff8432e4d7b7
Asaf Levi <asaf14levi@gmail.com>
2024-12-17 08:10:05 +0200

    adding drag to Deshboard

--- app/components/deshboard/BtnDeshboard.tsx
+++ app/components/deshboard/BtnDeshboard.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+export type BtnDeshboardType = {
+    srcImg: string,
+    srcColor: string
+}
 
-const BtnDeshboard = () => {
+const BtnDeshboard = ({srcImg, srcColor}: BtnDeshboardType) => {
   return (
     <div className="relative w-36 h-16 z-40">
-        <img src='/BtnColor.png' className='absolute object-contain z-50 mix-blend-color'/>
-        <img src='/BtnEngine.png' className='absolute object-contain'/>
+        <img src={srcColor} className='absolute object-contain z-50 mix-blend-color'/>
+        <img src={srcImg} className='absolute object-contain'/>
     </div>
   )
 }
--- app/components/deshboard/Deshboard.tsx
+++ app/components/deshboard/Deshboard.tsx
@@ -1,17 +1,18 @@
+import {motion} from 'framer-motion'
 import BtnDeshboard from "./BtnDeshboard"
 
 
 const Deshboard = () => {
   return (
-    <div className="w-[10%] h-[40%] z-20 relative flex flex-row justify-center">
+    <motion.div drag dragMomentum={false} className="w-[10%] h-[40%] z-20 relative flex flex-row justify-center">
        <div className="absolute w-full h-full flex justify-center">
             <img  src='/Deshboard.png' className="h-full"/>
        </div>
        <div className="relative flex flex-col space-y-7 top-10 right-1">
-            <BtnDeshboard />
-            <BtnDeshboard />
+            <BtnDeshboard srcColor="/BtnColor.png" srcImg="/btnEngine.png"/>
+            <BtnDeshboard srcColor="/BtnColor.png" srcImg="btnFire.png"/>
         </div>
-    </div>
+    </motion.div>
   )
 }
 
